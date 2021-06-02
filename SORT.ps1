4dc3b347030cb5110fdd451b0040a772a38f4208
hunterdavidson <hunter.c.davidson@gmail.com>
2021-06-02 07:30:22 -0500

    Update SORT.ps1

--- SORT.ps1
+++ SORT.ps1
@@ -1,13 +1,13 @@
 $workingDir = Get-Location
 
 ####Investor Fund Name
-$InvestorFundName = "New Era Medical Investment Fund IV"
+$InvestorFundName = "Investor Fund"
 
 
 foreach($folder in Get-ChildItem $workingDir\InvestorFolders) {
     $currentInvestorName = $folder.ToString().Split("-", 2)[1].Trim()
 
-    Get-ChildItem –Path $workingDir\InvestorFiles -Recurse -Filter *-$InvestorFundName-$currentInvestorName.pdf | Foreach-Object {Copy-Item $_.FullName -Destination $workingDir\InvestorFolders\$folder}
+    Get-ChildItem â€“Path $workingDir\InvestorFiles -Recurse -Filter *-$InvestorFundName-$currentInvestorName.pdf | Foreach-Object {Copy-Item $_.FullName -Destination $workingDir\InvestorFolders\$folder}
 }
 
 Write-Host "Sorting Completed"
