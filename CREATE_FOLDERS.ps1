633498e6d4b5f712ae475b9cf1c690eea6123f2c
hunterdavidson <hunter.c.davidson@gmail.com>
2021-06-24 12:00:34 -0500

    Bug Fixes

--- CREATE_FOLDERS.ps1
+++ CREATE_FOLDERS.ps1
@@ -1,15 +1,15 @@
 ####Enter starting row (rowA) and ending row (rowB)
-$rowA = 0
-$rowB = 0
+$rowA = 1
+$rowB = 16
 
 ####Enter starting collumn (collumnA) and ending collumn (collumnB)
-$collumnA = 0
-$collumnB = 0
+$collumnA = 1
+$collumnB = 3
 
 function End-Program {
      $excel.Quit()
      [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
-     Remove-Variable excel
+     Remove-Variable $excel
 
      Write-Host "Folder Creation Completed"
      cmd /c 'pause'
@@ -43,7 +43,9 @@ for($i=$rowA;$i-le $rowB;$i++){
 
         if($folderName -eq "") {
             Write-Host Missing data at collumn [$a], row[$i]
+	    $folderName = ""
             Ask-Continue
+	    continue
         }
 
         if($a -lt $collumnB){
@@ -54,11 +56,17 @@ for($i=$rowA;$i-le $rowB;$i++){
      if($folderName -eq "") {
             Write-Host Missing data at row[$i] -> Folder not created
             Ask-Continue
+	    $folderName = ""
             continue
         }
     
      
-     New-Item -path $workingDir\InvestorFolders\$folderName -ItemType "directory"
+     try { New-Item -path $workingDir\InvestorFolders\$folderName -ItemType "directory" }
+     catch {
+	    Write-Host Folder has already been created or could not be created because of an error
+        continue
+     }
+
      $folderName = ""
 }
 
